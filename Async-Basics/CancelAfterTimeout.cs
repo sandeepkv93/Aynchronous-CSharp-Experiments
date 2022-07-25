a8e8b64721edd36fad1b8856559d9102ec8c1d35
Sandeep Vishnu <savishnu@microsoft.com>
2022-07-25 14:03:04 -0700

    CancelAfterTimeout

---
+++ Async-Basics/CancelAfterTimeout.cs
@@ -0,0 +1,19 @@
+﻿namespace AsyncBasics
+{
+    public class CancelAfterTimeout
+    {
+        private int CancelAfterSeconds { get; set; }
+
+        public CancelAfterTimeout(int cancelAfterSeconds)
+        {
+            this.CancelAfterSeconds = cancelAfterSeconds;
+        }
+
+        public async Task IssueTimeoutAsync()
+        {
+            using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(CancelAfterSeconds));
+            CancellationToken token = cts.Token;
+            await Task.Delay(TimeSpan.FromSeconds(10), token);
+        }
+    }
+}
--- Async-Basics/Program.cs
+++ Async-Basics/Program.cs
@@ -5,6 +5,12 @@
 // This is a common scenario when unit testing or implementing retry delays.
 // It also comes up when coding simple timeouts.
 
-DelayResultByAPeriodOfTime delayResultByAPeriodOfTime = new DelayResultByAPeriodOfTime(delayInSeconds: 5);
-int sum = await delayResultByAPeriodOfTime.AddTwoNumbersWithDelay(10, 20);
-Console.WriteLine(sum);
+//DelayResultByAPeriodOfTime delayResultByAPeriodOfTime = new DelayResultByAPeriodOfTime(delayInSeconds: 5);
+//int sum = await delayResultByAPeriodOfTime.AddTwoNumbersWithDelay(10, 20);
+//Console.WriteLine(sum);
+
+// Problem Statement 2
+// You have some code that needs to stop running after a timeout.
+
+CancelAfterTimeout cancelAfterTimeout = new CancelAfterTimeout(cancelAfterSeconds: 5);
+await cancelAfterTimeout.IssueTimeoutAsync();
