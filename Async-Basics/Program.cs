2194378291ce698cc898643ef00fa77ac10beb80
Sandeep Vishnu <savishnu@microsoft.com>
2022-08-31 11:21:12 -0700

    Waiting for Multiple tasks and getting the result

--- Async-Basics/Program.cs
+++ Async-Basics/Program.cs
@@ -12,7 +12,17 @@
 // Problem Statement 2
 // You have some code that needs to stop running after a timeout.
 
-CancelAfterTimeout cancelAfterTimeout = new CancelAfterTimeout(cancelAfterSeconds: 5);
-//await cancelAfterTimeout.IssueTimeoutAsync();
-string? concatenatedString = await cancelAfterTimeout.ConcatenateStringsWithTimeout(new string[] { "The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog" });
-Console.WriteLine(concatenatedString);
+//CancelAfterTimeout cancelAfterTimeout = new CancelAfterTimeout(cancelAfterSeconds: 5);
+////await cancelAfterTimeout.IssueTimeoutAsync();
+//string? concatenatedString = await cancelAfterTimeout.ConcatenateStringsWithTimeout(new string[] { "The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog" });
+//Console.WriteLine(concatenatedString);
+
+// Problem Statement 3
+// Awaiting for multiple async calls together and getting the results
+//WaitingForMultipleTasks multipleTasks = new WaitingForMultipleTasks(delayInSeconds: 5);
+//Task<int> addTask = multipleTasks.AddNumbersWithDelay(10, 20);
+//Task<int> subtractTask = multipleTasks.SubtractNumbersWithDelay(25, 10);
+//Task<int> multiplyTask = multipleTasks.MultiplyNumbersWithDelay(10, 20);
+
+//await Task.WhenAll(addTask, subtractTask, multiplyTask);
+//Console.WriteLine($"Add: {addTask.Result}, Subtract: {subtractTask.Result}, Multiply: {multiplyTask.Result}");
---
+++ Async-Basics/WaitingForMultipleTasks.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncBasics
+{
+    public class WaitingForMultipleTasks
+    {
+        private readonly int delayInSeconds;
+
+        public WaitingForMultipleTasks(int delayInSeconds)
+        {
+            this.delayInSeconds = delayInSeconds;
+        }
+
+        public async Task<int> AddNumbersWithDelay(int firstNumber, int secondNumber)
+        {
+            await Task.Delay(this.delayInSeconds * 1000);
+            return firstNumber + secondNumber;
+        }
+
+        public async Task<int> SubtractNumbersWithDelay(int firstNumber, int secondNumber)
+        {
+            await Task.Delay(this.delayInSeconds * 1000);
+            return firstNumber - secondNumber;
+        }
+
+        public async Task<int> MultiplyNumbersWithDelay(int firstNumber, int secondNumber)
+        {
+            await Task.Delay(this.delayInSeconds * 1000);
+            return firstNumber * secondNumber;
+        }
+    }
+}
